ba63bbcf0249c916ceccc1bd99789fd82224db2d
threelittlemonkeys <sjlee7642@gmail.com>
2020-05-12 16:00:46 +0900

    Update by commit

--- parameters.py
+++ parameters.py
@@ -2,4 +2,4 @@ LANG = "vi"
 if LANG == "ko": NGRAM_SIZES = (1, 2, 3, 4, 5)
 if LANG == "vi": NGRAM_SIZES = (1, 2, 3)
 THRESHOLD = 1
-DEBUG = True
+DEBUG = False
--- utils.py
+++ utils.py
@@ -7,10 +7,11 @@ from parameters import *
 # 00C0-00FF (Latin-1 Supplement)
 # 0100-017F (Latin Extended-A)
 # 0180-024F (Latin Extended-B)
+# 0300-036F (Combining Diacritical Marks)
 # 1E00-1EFF (Latin Extended Additional)
 # AC00-D7AF (Hangul Syllables)
 
-RE_NON_ALNUM = re.compile("([^0-9A-Za-z\u00C0-\u024F\u1E00-\u1EFF\uAC00-\uD7AF]+)")
+RE_NON_ALNUM = re.compile("([^0-9A-Za-z\u00C0-\u024F\u0300-\u036F\u1E00-\u1EFF\uAC00-\uD7AF]+)")
 RE_CJK = re.compile("([\u4E00-\u9FFF\uAC00-\uD7AF])")
 
 def normalize(x, lc = True):
